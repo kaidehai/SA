b8d24f2884d1436c5e9984ff5598b963b6b8b3f6
Christian257 <81424701+Christian257@users.noreply.github.com>
2022-02-21 10:18:45 +0100

    funktioniert noch

---
+++ Code/MV_Inserts.sql
@@ -0,0 +1,9 @@
+INSERT INTO TVereine (VerName)
+VALUES ('SLRG'), ('Jugendmusik'); 
+
+INSERT INTO TMitglieder (MitVorname,MitNachname,MitGeburtstag,VerID)
+VALUES ('Christian','Köhler','2003-06-10','1001'), ('Katalin','Köhler','2003-06-10','1002'); 
+
+
+select * from  TVereine;
+select * from  TMitglieder;
